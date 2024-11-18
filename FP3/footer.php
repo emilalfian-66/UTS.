1bd7ff6c35fb78c930c9d71fc7f59c63223a7939
emil alfian <if23.emilalfian@mhs.ubpkarawang.ac.id>
2024-11-18 21:57:05 +0700

    Add files via upload

---
+++ FP3/footer.php
@@ -0,0 +1,17 @@
+<footer>
+  <style>
+    footer {
+      /* Warna latar belakang footer */
+      padding: 20px 0;
+      /* Tambahkan padding untuk footer */
+    }
+  </style>
+  <div class="content">
+    <div class="social-icons mt-3">
+      <a href="#" class="mx-2"><i class="fab fa-facebook-f"></i></a>
+      <a href="#" class="mx-2"><i class="fab fa-twitter"></i></a>
+      <a href="#" class="mx-2"><i class="fab fa-instagram"></i></a>
+      <a href="#" class="mx-2"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+  </div>
+</footer>
